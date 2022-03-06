d456a21c801229b5fce5f0f8f41e2d6a0eb7d181
goflvhxj2547 <goflvhxj2547@gmail.com>
2022-03-06 12:29:05 +0900

    🔬Chore:Nestjs 시작

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(3000);
+}
+
+bootstrap();
--- src/models/award.js
+++
@@ -1,8 +0,0 @@
-export default class Award {
-  constructor(imageData, creation, template, reciver) {
-    this.imageData = imageData;
-    this.creation = creation;
-    this.template = template;
-    this.reciver = reciver;
-  }
-}
